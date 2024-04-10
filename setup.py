3f805279d7df5d98ee0cca5489f3d2daee749129
dephinate <dephinate@pop-os.mynetworksettings.com>
2024-04-09 21:39:31 -0700

    updated setup.py - name of package changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 __version__ = "0.0.0"
 
-SRC_REPO = "MisterRetriveRight" # Source repo name
+SRC_REPO = "MedicalChatBot" # Source repo name
 REPO_NAME = "MedicalChatBot" # Github repo name
 AUTHOR_NAME = "Palpendiculal" # Github name
 AUTHOR_EMAIL = "varuns.india@gmail.com" # Github email
@@ -11,18 +11,18 @@ with open ("README.md", "r", encoding= "utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name= SRC_REPO,
-    version= __version__,
-    description= "A samll python package for RAG implimentation using langchain, PineCone, and Flask for NLP applications",
-    long_description= long_description,
+    name=SRC_REPO,  # Update package name here
+    version=__version__,
+    description="A samll python package for RAG implimentation using langchain, PineCone, and Flask for NLP applications",
+    long_description=long_description,
     
-    author= AUTHOR_NAME,
-    author_email= AUTHOR_EMAIL,
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
     
-    url= f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
-    project_urls = {
-    "Bug Tracker":f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
+    url=f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
     },
-    packages= setuptools.find_packages(where="src"),
-    package_dir= {"":"src"}
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"}
 )
