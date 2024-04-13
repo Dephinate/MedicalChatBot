79765740ad5561ff10f5ce577daf777f519cc1bc
dephinate <dephinate@pop-os.mynetworksettings.com>
2024-04-13 16:56:46 -0700

    added prompt templates

---
+++ src/medicalChatBot/prompts/__init__.py
@@ -0,0 +1,41 @@
+# Template String
+template1="""
+[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+Use the Context to answer the Question.
+Context: {context}
+Question: {question}[/INST]
+
+"""
+
+template2="""
+Answer the question based on the context below.
+If you don't know the answer, only return I don't know, Do not make an answer.
+
+Context: {context}
+Question: {question}
+
+Answer:
+"""
+
+
+template3 = """
+<<USER>> {question}
+
+<<SYS>>
+Answer the question based on the context below..
+{context}
+system_prompt: "You are a helpful assistant, below is ... llama-2-7b-chat.Q4_K_M.gguf"
+"""
+
+# Most effective Template String
+
+default_template="""
+[INST] <<SYS>>
+Answer the query based on the information. If you don't know the answer, return 'I do not know', Do not make an answer.
+<</SYS>>
+query:{question}
+information:{context}[/INST]
+
+"""
